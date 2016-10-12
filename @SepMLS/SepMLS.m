ef7d500e1c409b765db73b00aefc16a1a1afc9d5
Stanley Smith <swsmth@berkeley.edu>
2016-10-12 13:42:42 -0700

    finish most fns concerning partition space

--- @SepMLS/SepMLS.m
+++ @SepMLS/SepMLS.m
@@ -43,6 +43,8 @@ classdef SepMLS
         xpart_dim; % partition dimensions (number of cells by number of cells ...)
         xpart_prod; % for partition calculations
         xpart_offset; % x-value of partition coordinate [0 0 0 0 ... 0]'
+        % NOTE that this offset corresponds to the lowest corner of our
+        % partition hyperbox
         
         % input space partition
         upart_res;
@@ -53,23 +55,28 @@ classdef SepMLS
     
     methods
         
-        function ss = SepMLS(sub_n, part_res, part_dim, part_offset)
+        function ss = SepMLS(sub_n, xpart_info, upart_info)
+            
             if(size(part_dim, 2) ~= 1)
-                error('Please enter partition dimensions as a column vector.');
+                error('Error: partition dimensions should be a column vector.');
             end
             
             if(size(sub_num, 2) ~= 1)
-                error('Please enter subsystem dimensions as a column vector.');
+                error('Error: subsystem dimensions should be a column vector.');
             end
             
             if(sum(sub_num) ~= length(part_dim))
-                error('Dimension of partition space should equal sum of subsystem dimensions.');
+                error('Error: dimension of partition space should equal sum of subsystem dimensions.');
             end
             
             ss.sub_n = sub_n;
-            ss.xpart_res = part_res;
-            ss.xpart_dim = part_dim;
-            ss.xpart_offset = part_offset;
+            ss.xpart_res = xpart_info{1};
+            ss.xpart_dim = xpart_info{2};
+            ss.xpart_offset = xpart_info{3};
+            
+            ss.upart_res = upart_info{1};
+            ss.upart_dim = upart_info{2};
+            ss.upart_offset = upart;
             
             ss.xpart_prod = zeros(size(part_dim));
             for i = 1:length(part_dim)
@@ -77,15 +84,23 @@ classdef SepMLS
             end
         end
         
-        function in = inPart(ss, pcrd)
-        % note: this fn is only needed for x coordinates
+        function ovf = overflow(ss, pcrd, mode)
+        % check if a partition coordinate is inside the partition space
+        	if(strcmp(mode, 'state'))
+                part_dim = ss.xpart_dim;
+            elseif(strcmp(mode, 'input'))
+                part_dim = ss.upart_dim;
+            else
+                error('Error: mode specified should either be state or input.');
+            end
+            
             if(sum(mod(pcrd, 1)) ~= 0)
                 error('Error: expected integer partition coordinates.');
             end
-            if(size(pcrd) ~= size(ss.xpart_dim))
+            if(size(pcrd) ~= size(part_dim))
                 error('Error: unexpected partition coordinate dimensions.');
             end
-            in = (sum(zeros(3,1) <= pcrd <= ss.xpart_dim) == length(ss.xpart_dim));
+            ovf = ~(sum((zeros(3,1) <= pcrd) && (pcrd <= part_dim)) == length(part_dim));
         end
         
         function pidx = ptoi(ss, pcrd, mode)
@@ -103,15 +118,14 @@ classdef SepMLS
         
         function pcrd = itop(ss, pidx, mode)
         % partition index -> partition coordinate
-        
-        % possibly check if partition index is out of range
+            if(
         
             if(strcmp(mode, 'state'))
                 part_prod = ss.xpart_prod;
             elseif(strcmp(mode, 'input'))
                 part_prod = ss.upart_prod;
             else
-                error('Error: mode should be a string, either "state" or "input".');
+                error('Error: mode should be a string, either state or input.');
             end
         
             pcrd = zeros(size(part_prod));
@@ -124,13 +138,15 @@ classdef SepMLS
         function sscrd = ptox(ss, pidx, mode)
         % partition coordinate -> state space coordinate
             if(strcmp(mode, 'state'))
-                part_prod = ss.xpart_prod;
+                part_offset = ss.xpart_offset;
+                part_res = ss.xpart_res;
             elseif(strcmp(mode, 'input'))
-                part_prod = ss.upart_prod;
+                part_offset = ss.upart_offset;
+                part_res = ss.upart_res;
             else
-                error('Error: mode should be a string, either "state" or "input".');
+                error('Error: mode should be a string, either state or input.');
             end
-            
+            sscrd = pidx*part_res + part_offset;
         end
             
         function pcrd = xtop(ss, sscrd, mode)
